e938dc027ad3f0a1f76058498842e3ecf8e61ad1
Trevor <tjd2695@rit.edu>
2024-05-29 13:12:41 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const createRoom = (e) => {
+    console.log("Called!");
+
+    const name = document.querySelector("#roomName").value;
+    const theme = document.querySelector("#roomTheme").value;
+    const members = document.querySelector("#roomMembers").value;
+
+    console.log("Name: " + name);
+    console.log("Theme: " + theme);
+    console.log("Members: " + members);
+};
+
+const init = () => {
+    const button = document.querySelector("#roomSubmit");
+
+    button.onclick = (e) => {
+        createRoom(e);
+    };
+};
+window.onload = init;
