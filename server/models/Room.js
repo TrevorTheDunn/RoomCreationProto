a35a87602f4992ca3dfdf9f01dd35843f950de5e
TrevorTheDunn <tjd2695@rit.edu>
2024-06-05 10:24:06 -0400

    Fixed eslint

--- server/models/Room.js
+++ server/models/Room.js
@@ -5,32 +5,32 @@ const setName = (name) => _.escape(name).trim();
 const setTheme = (theme) => _.escape(theme).trim();
 
 const RoomSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true,
-        set: setName,
-    },
-    theme: {
-        type: String,
-        required: true,
-        trim: true,
-        set: setTheme,
-    },
-    amtMembers: {
-        type: Number,
-        required: true,
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  theme: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setTheme,
+  },
+  amtMembers: {
+    type: Number,
+    required: true,
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 RoomSchema.statics.toAPI = (doc) => ({
-    name: doc.name,
-    theme: doc.theme,
-    amtMembers: doc.amtMembers,
+  name: doc.name,
+  theme: doc.theme,
+  amtMembers: doc.amtMembers,
 });
 
 const RoomModel = mongoose.model('Room', RoomSchema);
--- server/router.js
+++ server/router.js
@@ -1,16 +1,14 @@
 const controllers = require('./controllers');
 
 const router = (app) => {
+  app.get('/getRoomData', controllers.Room.getRoomData);
 
-    app.get('/getRoomData', controllers.Room.getRoomData);
+  app.get('/roomView', controllers.Room.roomPage);
 
-    app.get('/roomView', controllers.Room.roomPage);
+  app.post('/createRoom', controllers.Room.createRoom);
 
-    app.post('/createRoom', controllers.Room.createRoom);
-
-    app.get('/roomCreator', controllers.Room.creatorPage);
-    app.get('/', controllers.Room.creatorPage);
-
+  app.get('/roomCreator', controllers.Room.creatorPage);
+  app.get('/', controllers.Room.creatorPage);
 };
 
 module.exports = router;
